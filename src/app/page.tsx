edd8655faca96b230e6716dd4ca08eb37d2db6fa
lleung030 <lucasleung2593@gmail.com>
2023-07-06 09:39:28 -0700

    instagram stories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from '../components/header';
+import Header from '../components/Header';
+import Feed from '../components/Feed';
+
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>Instagram 2.0</title>
         <link rel="icon" href="/favicon.icon" />
       </Head>
 
-      {/* Header */}
       <Header />
-
-      {/* Feed */}
+      <Feed />
 
       {/* Modal */}
 
